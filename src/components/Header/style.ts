1bc4ab36dc7473d5371ee640311718e0ac62b16e
dalissongabriel <alissongabrieldino@gmail.com>
2021-05-18 19:57:41 -0300

    feat: make header component

---
+++ src/components/Header/style.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    background: var(--azul)
+`;
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 2rem 1rem 13rem;
+
+    button {
+        color: #fff;
+        font-size: 1rem;
+        font-weight: 600;
+        
+        padding: 0 2rem;
+        height: 3rem;
+        background: var(--azul-claro);
+        border-radius: .4rem;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
+`;
