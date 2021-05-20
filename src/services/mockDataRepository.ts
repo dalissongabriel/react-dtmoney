ac80569d7b56b59528cf1b5c55e76d1be59483d9
dalissongabriel <alissongabrieldino@gmail.com>
2021-05-19 21:16:20 -0300

    chore: add axios and config the api transactions data

---
+++ src/services/mockDataRepository.ts
@@ -0,0 +1,26 @@
+export const transactionsData = [
+    {
+        id:1,
+        title: "Desenvolvimento de site",
+        amount: 1000,
+        type: 'deposity',
+        category: 'Freela',
+        createdAt: new Date()
+    },
+    {
+        id:1,
+        title: "Horas Extras",
+        amount: 200,
+        type: 'deposity',
+        category: 'CLT',
+        createdAt: new Date()
+    },
+    {
+        id:1,
+        title: "Aluguel",
+        amount: 400,
+        type: 'withdraw',
+        category: 'Custos Fixos',
+        createdAt: new Date()
+    }
+]
