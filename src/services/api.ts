13973a6e3685ba24755fc45439719e626638c89f
dalissongabriel <alissongabrieldino@gmail.com>
2021-07-04 01:59:16 -0300

    🐛 fix: correct env variable for build step in vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: `http://${process.env.VERCEL_URL ?? 'localhost:3000'}/api`,
+    baseURL: `http://${process.env.REACT_APP_VERCEL_URL ?? 'localhost:3000'}/api`,
 });
