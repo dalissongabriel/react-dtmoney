b9feac4242c521fbc8ca520dff47e705a30b41e5
dalissongabriel <alissongabrieldino@gmail.com>
2021-07-04 03:06:27 -0300

    🚀 feat: add miragejs url for production enviroment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const url = process.env.REACT_APP_MIRAGEJS_URL
+    ?? process.env.REACT_APP_VERCEL_URL
+    ?? 'localhost:3000';
+
 export const api = axios.create({
-    baseURL: `http://${process.env.REACT_APP_VERCEL_URL ?? 'localhost:3000'}/api`,
+    baseURL: `http://${url}/api`,
 });
