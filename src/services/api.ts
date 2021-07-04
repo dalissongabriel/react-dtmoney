76a7e04be1634b8b726c5ced6ff41ef86136d914
dalissongabriel <alissongabrieldino@gmail.com>
2021-07-04 01:52:09 -0300

    🐛 fix: adjust axios base url  for miragejs work

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api", 
+    baseURL: `http://${process.env.VERCEL_URL ?? 'localhost:3000'}/api`,
 });
